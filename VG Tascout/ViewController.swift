dd8fd063e4ec2bc2422734f8be2dcae1c90bc05b
Marius Gabriel  Magadan <Wakirox@mariuss-mac.local>
2016-03-29 14:58:50 +0200

    Initial Commit

---
+++ VG Tascout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VG Tascout
+//
+//  Created by Marius Gabriel  Magadan on 29/03/16.
+//  Copyright © 2016 Tascout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
